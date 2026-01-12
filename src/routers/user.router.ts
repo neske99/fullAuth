e79b1b4ab9bfa37647110e84f036211ca53352cd
neske99 <nnesic172@gmail.com>
2026-01-12 18:18:00 +0100

    fix: setUserRoles now return true if succeded update

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,12 +2,15 @@
 import express from 'express'
 import authMiddleware, { authorizeRoles } from '../middlewares/auth.middleware.ts';
 import { Role } from '../constants/roles.ts';
+import { setUserRoles } from '../services/user.service.ts';
 
 const router=express.Router();
 
-router.post('setUserRoles',authMiddleware,authorizeRoles(Role.Admin),function(req,res,next){
-  res.sendStatus(200);
+router.post('/setUserRoles',authMiddleware,authorizeRoles(Role.Admin),async function(req,res,next){
+  res.send(await setUserRoles(req.body.username,req.body.roles));
 });
+
+
 
 
 export default router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,20 +1,23 @@
 import {userModel} from '../models/user.model.ts'
 import type {User} from '../models/user.model.ts'
 import {Role} from '../constants/roles.ts'
-/*
+
 const setUserRoles=async function(username:string,roles:Role[]){
+  console.log(username,roles)
   if(roles.includes(Role.Admin))
     return null
-  const user= await userModel.findOne({username:username}).exec();
+  const user:User | null= await userModel.findOne({username:username}).exec();
   if(user==null){
-    return null;
+    return false;
   }
-  if(user.roles.includes(Role.Admin))
-    return null
-  else{
-    let res= await user.updateOne({roles:roles}).exec();
-  }
-}
-  */
 
-//export {setUserRoles}
+  if((user.roles.includes(Role.Admin) && user.username!=username) )
+    return false;
+  else{
+    let result=await userModel.updateOne({username:username},{roles:roles}).exec();
+    console.log(result);
+    return result.acknowledged && result.matchedCount>0;
+  }
+  }
+
+export {setUserRoles}
